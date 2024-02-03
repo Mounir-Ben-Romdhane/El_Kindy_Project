22541def4247c501f11f51d10170356811b48707
Mounir Ben Romdhane <mounir.benromdhane@esprit.tn>
2024-02-03 21:03:32 +0100

    test

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import AboutPage from '../src/scenes/AboutPage'
+import BlogPage from '../src/scenes/BlogPage'
+import ContactPage from '../src/scenes/ContactPage'
+import HomePage from '../src/scenes/HomePage'
+import ServicesPage from '../src/scenes/ServicesPage'
+import ShopPage from '../src/scenes/ShopPage'
+import {BrowserRouter, Routes, Route } from "react-router-dom";
+import NavBar from './components/NavBar';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div>
+        <NavBar/>
+        <Routes>
+          <Route path="/" element={<HomePage />}/>
+          <Route path="/about" element={<AboutPage />}/>
+          <Route path="/blog" element={<BlogPage />}/>
+          <Route path="/contact" element={<ContactPage />}/>
+          <Route path="/services" element={<ServicesPage />}/>
+          <Route path="/shop" element={<ShopPage />}/>
+        </Routes>
+        <Footer/>
+      </div>
+  )
 }
 
-export default App;
+export default App
