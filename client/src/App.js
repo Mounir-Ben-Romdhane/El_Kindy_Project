d0575deb324b6503c94b93d724c760720ab513c5
Mounir Ben Romdhane <mounir.benromdhane@esprit.tn>
2024-02-18 00:24:38 +0100

    fix errors

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ import ListCategoryPage from '../src/scenes/Category/ListCategoryPage'
 import AddCategoryPage from '../src/scenes/Category/AddCategoryPage'
 import EditCategoryPage from '../src/scenes/Category/EditCategoryPage'
 
-import {BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import { Navigate, Routes, Route } from "react-router-dom";
 
 import { useSelector } from "react-redux";
 import { loadScripts } from './scriptLoader';
@@ -28,34 +28,6 @@ function App() {
 
   const isAuth = Boolean(useSelector((state) => state.token));
   
-  const scriptsLoaded = useRef(false);
-
-  useEffect(() => {
-    const scripts = [
-      '/assets/vendor/bootstrap/dist/js/bootstrap.bundle.min.js',
-      '/assets/vendor/tiny-slider/tiny-slider.js',
-      '/assets/vendor/glightbox/js/glightbox.js',
-      '/assets/vendor/purecounterjs/dist/purecounter_vanilla.js',
-      '/assets/vendor/choices/js/choices.min.js',
-      '/assets/vendor/aos/aos.js',
-      '/assets/vendor/quill/js/quill.min.js',
-      '/assets/vendor/stepper/js/bs-stepper.min.js',
-      '/assets/js/functions.js',
-    ];
-
-    if (!scriptsLoaded.current) {
-      loadScripts(scripts);
-      scriptsLoaded.current = true;
-    }
-
-    return () => {
-      // Remove all script tags
-      const scriptTags = document.querySelectorAll('script[src^="/assets"]');
-      scriptTags.forEach((scriptTag) => {
-        scriptTag.parentNode.removeChild(scriptTag);
-      });
-    };
-  }, []);
 
 
   return (
@@ -66,7 +38,7 @@ function App() {
           <Route path="/" element={<SignIn />} />
           <Route path="/signup" element={<SignUp />} />
           <Route path="/forgot-password" element={<ForgetPassword />} />
-          <Route path="/reset-password/:id/:token" element={<ResetPassword />} />
+          <Route path="/reset-password/:id?/:token?" element={<ResetPassword />} />
           <Route path="/verify-account/:id/verify/:token" element={<EmailVerify />} />
           <Route 
               path="/home" 
